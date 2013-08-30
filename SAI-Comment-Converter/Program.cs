d642c4e3aad49d533a0e176b4f0009e9979d3e9b
Discover- <amort11@hotmail.com>
2013-08-30 18:56:34 +0200

    Continuing on desktop PC

--- SAI-Comment-Converter/Program.cs
+++ SAI-Comment-Converter/Program.cs
@@ -7,6 +7,144 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
+enum SMARTAI_TARGETS
+{
+    SMART_TARGET_NONE                           = 0,    // NONE, defaulting to invoket
+    SMART_TARGET_SELF                           = 1,    // Self cast
+    SMART_TARGET_VICTIM                         = 2,    // Our current target (ie: highest aggro)
+    SMART_TARGET_HOSTILE_SECOND_AGGRO           = 3,    // Second highest aggro
+    SMART_TARGET_HOSTILE_LAST_AGGRO             = 4,    // Dead last on aggro
+    SMART_TARGET_HOSTILE_RANDOM                 = 5,    // Just any random target on our threat list
+    SMART_TARGET_HOSTILE_RANDOM_NOT_TOP         = 6,    // Any random target except top threat
+    SMART_TARGET_ACTION_INVOKER                 = 7,    // Unit who caused this Event to occur
+    SMART_TARGET_POSITION                       = 8,    // x, y, z, o
+    SMART_TARGET_CREATURE_RANGE                 = 9,    // CreatureEntry(0any), minDist, maxDist
+    SMART_TARGET_CREATURE_GUID                  = 10,   // guid, entry
+    SMART_TARGET_CREATURE_DISTANCE              = 11,   // CreatureEntry(0any), maxDist
+    SMART_TARGET_STORED                         = 12,   // id, uses pre-stored target(list)
+    SMART_TARGET_GAMEOBJECT_RANGE               = 13,   // entry(0any), min, max
+    SMART_TARGET_GAMEOBJECT_GUID                = 14,   // guid, entry
+    SMART_TARGET_GAMEOBJECT_DISTANCE            = 15,   // entry(0any), maxDist
+    SMART_TARGET_INVOKER_PARTY                  = 16,   // invoker's party members
+    SMART_TARGET_PLAYER_RANGE                   = 17,   // min, max
+    SMART_TARGET_PLAYER_DISTANCE                = 18,   // maxDist
+    SMART_TARGET_CLOSEST_CREATURE               = 19,   // CreatureEntry(0any), maxDist, dead?
+    SMART_TARGET_CLOSEST_GAMEOBJECT             = 20,   // entry(0any), maxDist
+    SMART_TARGET_CLOSEST_PLAYER                 = 21,   // maxDist
+    SMART_TARGET_ACTION_INVOKER_VEHICLE         = 22,   // Unit's vehicle who caused this Event to occur
+    SMART_TARGET_OWNER_OR_SUMMONER              = 23,   // Unit's owner or summoner
+    SMART_TARGET_THREAT_LIST                    = 24,   // All units on creature's threat list
+    SMART_TARGET_CLOSEST_ENEMY                  = 25,   // maxDist
+    SMART_TARGET_CLOSEST_FRIENDLY               = 26,   // maxDist
+}
+
+enum SmartEventFlags
+{
+    SMART_EVENT_FLAG_NOT_REPEATABLE        = 0x001,                     //Event can not repeat
+    SMART_EVENT_FLAG_DIFFICULTY_0          = 0x002,                     //Event only occurs in instance difficulty 0
+    SMART_EVENT_FLAG_DIFFICULTY_1          = 0x004,                     //Event only occurs in instance difficulty 1
+    SMART_EVENT_FLAG_DIFFICULTY_2          = 0x008,                     //Event only occurs in instance difficulty 2
+    SMART_EVENT_FLAG_DIFFICULTY_3          = 0x010,                     //Event only occurs in instance difficulty 3
+    SMART_EVENT_FLAG_RESERVED_5            = 0x020,
+    SMART_EVENT_FLAG_RESERVED_6            = 0x040,
+    SMART_EVENT_FLAG_DEBUG_ONLY            = 0x080,                     //Event only occurs in debug build
+    SMART_EVENT_FLAG_DONT_RESET            = 0x100,                     //Event will not reset in SmartScript::OnReset()
+
+    SMART_EVENT_FLAG_DIFFICULTY_ALL        = (SMART_EVENT_FLAG_DIFFICULTY_0|SMART_EVENT_FLAG_DIFFICULTY_1|SMART_EVENT_FLAG_DIFFICULTY_2|SMART_EVENT_FLAG_DIFFICULTY_3),
+    SMART_EVENT_FLAGS_ALL                  = (SMART_EVENT_FLAG_NOT_REPEATABLE|SMART_EVENT_FLAG_DIFFICULTY_ALL|SMART_EVENT_FLAG_RESERVED_5|SMART_EVENT_FLAG_RESERVED_6|SMART_EVENT_FLAG_DEBUG_ONLY|SMART_EVENT_FLAG_DONT_RESET)
+}
+
+enum SMART_EVENT_PHASE
+{
+    SMART_EVENT_PHASE_ALWAYS  = 0,
+    SMART_EVENT_PHASE_1       = 1,
+    SMART_EVENT_PHASE_2       = 2,
+    SMART_EVENT_PHASE_3       = 3,
+    SMART_EVENT_PHASE_4       = 4,
+    SMART_EVENT_PHASE_5       = 5,
+    SMART_EVENT_PHASE_6       = 6,
+    SMART_EVENT_PHASE_MAX     = 7,
+}
+
+enum SMART_EVENT
+{
+    SMART_EVENT_UPDATE_IC                = 0,       // InitialMin, InitialMax, RepeatMin, RepeatMax
+    SMART_EVENT_UPDATE_OOC               = 1,       // InitialMin, InitialMax, RepeatMin, RepeatMax
+    SMART_EVENT_HEALT_PCT                = 2,       // HPMin%, HPMax%,  RepeatMin, RepeatMax
+    SMART_EVENT_MANA_PCT                 = 3,       // ManaMin%, ManaMax%, RepeatMin, RepeatMax
+    SMART_EVENT_AGGRO                    = 4,       // NONE
+    SMART_EVENT_KILL                     = 5,       // CooldownMin0, CooldownMax1, playerOnly2, else creature entry3
+    SMART_EVENT_DEATH                    = 6,       // NONE
+    SMART_EVENT_EVADE                    = 7,       // NONE
+    SMART_EVENT_SPELLHIT                 = 8,       // SpellID, School, CooldownMin, CooldownMax
+    SMART_EVENT_RANGE                    = 9,       // MinDist, MaxDist, RepeatMin, RepeatMax
+    SMART_EVENT_OOC_LOS                  = 10,      // NoHostile, MaxRnage, CooldownMin, CooldownMax
+    SMART_EVENT_RESPAWN                  = 11,      // type, MapId, ZoneId
+    SMART_EVENT_TARGET_HEALTH_PCT        = 12,      // HPMin%, HPMax%, RepeatMin, RepeatMax
+    SMART_EVENT_TARGET_CASTING           = 13,      // RepeatMin, RepeatMax, spellid
+    SMART_EVENT_FRIENDLY_HEALTH          = 14,      // HPDeficit, Radius, RepeatMin, RepeatMax
+    SMART_EVENT_FRIENDLY_IS_CC           = 15,      // Radius, RepeatMin, RepeatMax
+    SMART_EVENT_FRIENDLY_MISSING_BUFF    = 16,      // SpellId, Radius, RepeatMin, RepeatMax
+    SMART_EVENT_SUMMONED_UNIT            = 17,      // CreatureId(0 all), CooldownMin, CooldownMax
+    SMART_EVENT_TARGET_MANA_PCT          = 18,      // ManaMin%, ManaMax%, RepeatMin, RepeatMax
+    SMART_EVENT_ACCEPTED_QUEST           = 19,      // QuestID(0any)
+    SMART_EVENT_REWARD_QUEST             = 20,      // QuestID(0any)
+    SMART_EVENT_REACHED_HOME             = 21,      // NONE
+    SMART_EVENT_RECEIVE_EMOTE            = 22,      // EmoteId, CooldownMin, CooldownMax, condition, val1, val2, val3
+    SMART_EVENT_HAS_AURA                 = 23,      // Param1 = SpellID, Param2 = Stack amount, Param3/4 RepeatMin, RepeatMax
+    SMART_EVENT_TARGET_BUFFED            = 24,      // Param1 = SpellID, Param2 = Stack amount, Param3/4 RepeatMin, RepeatMax
+    SMART_EVENT_RESET                    = 25,      // Called after combat, when the creature respawn and spawn.
+    SMART_EVENT_IC_LOS                   = 26,      // NoHostile, MaxRnage, CooldownMin, CooldownMax
+    SMART_EVENT_PASSENGER_BOARDED        = 27,      // CooldownMin, CooldownMax
+    SMART_EVENT_PASSENGER_REMOVED        = 28,      // CooldownMin, CooldownMax
+    SMART_EVENT_CHARMED                  = 29,      // NONE
+    SMART_EVENT_CHARMED_TARGET           = 30,      // NONE
+    SMART_EVENT_SPELLHIT_TARGET          = 31,      // SpellID, School, CooldownMin, CooldownMax
+    SMART_EVENT_DAMAGED                  = 32,      // MinDmg, MaxDmg, CooldownMin, CooldownMax
+    SMART_EVENT_DAMAGED_TARGET           = 33,      // MinDmg, MaxDmg, CooldownMin, CooldownMax
+    SMART_EVENT_MOVEMENTINFORM           = 34,      // MovementType(any), PointID
+    SMART_EVENT_SUMMON_DESPAWNED         = 35,      // Entry, CooldownMin, CooldownMax
+    SMART_EVENT_CORPSE_REMOVED           = 36,      // NONE
+    SMART_EVENT_AI_INIT                  = 37,      // NONE
+    SMART_EVENT_DATA_SET                 = 38,      // Id, Value, CooldownMin, CooldownMax
+    SMART_EVENT_WAYPOINT_START           = 39,      // PointId(0any), pathID(0any)
+    SMART_EVENT_WAYPOINT_REACHED         = 40,      // PointId(0any), pathID(0any)
+    SMART_EVENT_TRANSPORT_ADDPLAYER      = 41,      // NONE
+    SMART_EVENT_TRANSPORT_ADDCREATURE    = 42,      // Entry (0 any)
+    SMART_EVENT_TRANSPORT_REMOVE_PLAYER  = 43,      // NONE
+    SMART_EVENT_TRANSPORT_RELOCATE       = 44,      // PointId
+    SMART_EVENT_INSTANCE_PLAYER_ENTER    = 45,      // Team (0 any), CooldownMin, CooldownMax
+    SMART_EVENT_AREATRIGGER_ONTRIGGER    = 46,      // TriggerId(0 any)
+    SMART_EVENT_QUEST_ACCEPTED           = 47,      // none
+    SMART_EVENT_QUEST_OBJ_COPLETETION    = 48,      // none
+    SMART_EVENT_QUEST_COMPLETION         = 49,      // none
+    SMART_EVENT_QUEST_REWARDED           = 50,      // none
+    SMART_EVENT_QUEST_FAIL               = 51,      // none
+    SMART_EVENT_TEXT_OVER                = 52,      // GroupId from creature_text,  creature entry who talks (0 any)
+    SMART_EVENT_RECEIVE_HEAL             = 53,      // MinHeal, MaxHeal, CooldownMin, CooldownMax
+    SMART_EVENT_JUST_SUMMONED            = 54,      // none
+    SMART_EVENT_WAYPOINT_PAUSED          = 55,      // PointId(0any), pathID(0any)
+    SMART_EVENT_WAYPOINT_RESUMED         = 56,      // PointId(0any), pathID(0any)
+    SMART_EVENT_WAYPOINT_STOPPED         = 57,      // PointId(0any), pathID(0any)
+    SMART_EVENT_WAYPOINT_ENDED           = 58,      // PointId(0any), pathID(0any)
+    SMART_EVENT_TIMED_EVENT_TRIGGERED    = 59,      // id
+    SMART_EVENT_UPDATE                   = 60,      // InitialMin, InitialMax, RepeatMin, RepeatMax
+    SMART_EVENT_LINK                     = 61,      // INTERNAL USAGE, no params, used to link together multiple events, does not use any extra resources to iterate event lists needlessly
+    SMART_EVENT_GOSSIP_SELECT            = 62,      // menuID, actionID
+    SMART_EVENT_JUST_CREATED             = 63,      // none
+    SMART_EVENT_GOSSIP_HELLO             = 64,      // none
+    SMART_EVENT_FOLLOW_COMPLETED         = 65,      // none
+    SMART_EVENT_DUMMY_EFFECT             = 66,      // spellId, effectIndex
+    SMART_EVENT_IS_BEHIND_TARGET         = 67,      // cooldownMin, CooldownMax
+    SMART_EVENT_GAME_EVENT_START         = 68,      // game_event.Entry
+    SMART_EVENT_GAME_EVENT_END           = 69,      // game_event.Entry
+    SMART_EVENT_GO_STATE_CHANGED         = 70,      // go state
+    SMART_EVENT_GO_EVENT_INFORM          = 71,      // eventId
+    SMART_EVENT_ACTION_DONE              = 72,      // eventId (SharedDefines.EventId)
+    SMART_EVENT_ON_SPELLCLICK            = 73,      // clicker (unit)
+    SMART_EVENT_FRIENDLY_HEALTH_PCT      = 74      // minHpPct, maxHpPct, repeatMin, repeatMax
+}
+
 namespace SAI_Comment_Converter
 {
     class Program
@@ -29,9 +167,9 @@ namespace SAI_Comment_Converter
 
                 string host = "127.0.0.1";
                 string user = "root";
-                string pass = "1234";
+                string pass = "123";
                 string worldDB = "trinitycore_world";
-                string port = "3306";
+                string port = "3307";
 
                 //Console.WriteLine(host);
                 //Console.WriteLine(user);
@@ -61,7 +199,6 @@ namespace SAI_Comment_Converter
                         foreach (DataRow row in dataTable.Rows)
                         {
                             MySqlCommand command = new MySqlCommand();
-                            MySqlDataReader reader = null;
                             command.Connection = connection;
 
                             string fullLine = "UPDATE `smart_scripts` SET `comment`='";
@@ -71,30 +208,111 @@ namespace SAI_Comment_Converter
                             if (Convert.ToInt32(row.ItemArray[0].ToString()) < 0)
                             {
                                 command.CommandText = (String.Format("SELECT id FROM creature WHERE guid={0}", -entryorguid));
-                                reader = command.ExecuteReader(CommandBehavior.SingleResult);
+                                MySqlDataReader readerCreatureId = command.ExecuteReader(CommandBehavior.Default);
 
-                                if (reader.Read())
-                                    entry = Convert.ToInt32(reader[0].ToString());
+                                if (readerCreatureId.Read())
+                                    entry = Convert.ToInt32(readerCreatureId[0].ToString());
+
+                                readerCreatureId.Close();
                             }
 
-                            reader.Close();
                             command.CommandText = (String.Format("SELECT name FROM creature_template WHERE entry={0}", entry));
-                            reader = command.ExecuteReader(CommandBehavior.SingleResult);
+                            MySqlDataReader readerCreatureName = command.ExecuteReader(CommandBehavior.Default);
 
-                            if (reader.Read())
-                                fullLine = reader[0].ToString() + " - ";
+                            if (readerCreatureName.Read())
+                                fullLine = readerCreatureName[0].ToString() + " - ";
 
+                            readerCreatureName.Close();
                             Console.WriteLine(fullLine);
                             Console.ReadKey();
 
-                            //! event_type
-                            switch (Convert.ToInt32(row.ItemArray[4].ToString()))
-                            {
-                                
-                            }
+                            //! Event type
+                            fullLine += GetStringForEventType(Convert.ToInt32(row.ItemArray[4].ToString()));
+                            fullLine += " - ";
+                            fullLine += GetStringForEventType(Convert.ToInt32(row.ItemArray[4].ToString()));
                         }
                     }
                 }
+            }
+        }
+
+        private static string GetStringForEventType(int eventType)
+        {
+            switch (eventType)
+            {
+                case SMART_EVENT_UPDATE_IC:
+                    return "In Combat";
+                case SMART_EVENT_UPDATE_OOC:
+                    return "Out Of Combat";
+                case SMART_EVENT_AGGRO:
+                    return "On Aggro";
+                case SMART_EVENT_KILL:
+                    return "On Killed Unit";
+                case SMART_EVENT_DEATH:
+                    return "On Death";
+                case SMART_EVENT_EVADE:
+                    return "On Evade";
+                case SMART_EVENT_OOC_LOS:
+                    return "On LOS Out Of Combat";
+                case SMART_EVENT_IC_LOS:
+                    return "On LOS In Combat";
+                case SMART_EVENT_RESPAWN:
+                    return "On Respawn";
+                case SMART_EVENT_TARGET_CASTING:
+                    return "On Target Casting";
+                case SMART_EVENT_FRIENDLY_IS_CC:
+                    return "On Friendly Unit CC'd";
+                case SMART_EVENT_FRIENDLY_MISSING_BUFF:
+                    return "On Friendly Unit Missing Buff _spellNameFirstParam_";
+                case SMART_EVENT_SUMMONED_UNIT:
+                    return "On Summoned Unit";
+                case SMART_EVENT_ACCEPTED_QUEST:
+                    return "On Quest Accepted";
+                case SMART_EVENT_REWARD_QUEST:
+                    return "On Quest Rewarded";
+                case SMART_EVENT_REACHED_HOME:
+                    return "On Just Reached Home";
+                case SMART_EVENT_RECEIVE_EMOTE:
+                    return "On Received Emote";
+                case SMART_EVENT_TARGET_BUFFED:
+                    return "On Target Buffed";
+                case SMART_EVENT_RESET:
+                    return "On Reset";
+                case SMART_EVENT_PASSENGER_BOARDED:
+                    return "On Passenger Boarded";
+                case SMART_EVENT_PASSENGER_REMOVED:
+                    return "On Passenger Removed";
+                case SMART_EVENT_CHARMED:
+                    return "On Charmed";
+                case SMART_EVENT_CHARMED_TARGET:
+                    return "On Target Charmed";
+                case SMART_EVENT_MOVEMENTINFORM:
+                    return "On Movement Inform";
+                case SMART_EVENT_SUMMON_DESPAWNED:
+                    return "On Summoned Unit Despawned";
+                case SMART_EVENT_CORPSE_REMOVED:
+                    return "On Corpse Removed";
+                case SMART_EVENT_SPELLHIT:
+                    return "On Spellhit By _spellNameFirstParam_";
+                case SMART_EVENT_SPELLHIT_TARGET:
+                    return "On Target Spellhit By _spellNameFirstParam_";
+                case SMART_EVENT_RANGE:
+                    return "Between ${param1}-${param2} Range";
+                case SMART_EVENT_HEALT_PCT:
+                    return "Between ${param1}-${param2}% Health";
+                case SMART_EVENT_MANA_PCT:
+                    return "Between ${param1}-${param2}% Mana";
+                case SMART_EVENT_TARGET_HEALTH_PCT:
+                    return "On Target Between ${param1}-${param2}% Health";
+                case SMART_EVENT_TARGET_MANA_PCT:
+                    return "On Target Between ${param1}-${param2}% Mana";
+                case SMART_EVENT_FRIENDLY_HEALTH:
+                    return "On Friendly Unit At ${param1} Health Within ${param2} Range";
+                case SMART_EVENT_HAS_AURA:
+                    //if ($param1 < 0)
+                        return "On Aura _hasAuraSpellId_ Not Present";
+                    
+                    return "On Aura _hasAuraSpellId_ Present";
             }
         }
     }
