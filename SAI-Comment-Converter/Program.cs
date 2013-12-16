c3239b598765898718fbef2a853d11a16769334e
Discover- <amort11@hotmail.com>
2013-12-16 16:13:40 +0100

    Fix an issue where the loaded-script-counter would be incremented twice for scripts that had their comment not updated

--- SAI-Comment-Converter/Program.cs
+++ SAI-Comment-Converter/Program.cs
@@ -803,7 +803,7 @@ namespace SAI_Comment_Converter
                         //! Don't update the script if the comment is already correct
                         if (cleanNewComment == smartScript.comment)
                         {
-                            totalSkippedScripts++;
+                            totalSkippedScripts--;
                             continue;
                         }
 
