Anonymous2/SAI-Comment-Converter

commit 6b70222b2ce099cad04fd4c2cb33ce7e5565a08f

Fix comment generating for SMART_ACTION_INC_EVENT_PHASE; I previously misunderstood the action_type's parameters

SAI-Comment-Converter/Program.cs
@@ -402,13 +402,12 @@ namespace SAI_Comment_Converter
                         fullLine = fullLine.Replace("_enableDisableActionParamOne_", "Enable");
                 }
 
-                if (fullLine.Contains("_incrementOrDecrementActionParamOne_"))
+                if (fullLine.Contains("_incrementOrDecrementPhaseActionParamsOneAndTwo_"))
                 {
-                    if (smartscript.action_param1.ToString() == "1")
-                        fullLine = fullLine.Replace("_incrementOrDecrementActionParamOne_", "Increment");
-                    else if (smartscript.action_param2.ToString() == "1")
-                        fullLine = fullLine.Replace("_incrementOrDecrementActionParamOne_", "Decrement");
-                    //else //? What to do?
+                    if (smartscript.action_param1 != 0)
+                        fullLine = fullLine.Replace("_incrementOrDecrementPhaseActionParamsOneAndTwo_", "Increment Phase By " + smartscript.action_param2);
+                    else if (smartscript.action_param2 != 0)
+                        fullLine = fullLine.Replace("_incrementOrDecrementPhaseActionParamsOneAndTwo_", "Decrement Phase By " + smartscript.action_param2);
                 }
 
                 if (fullLine.Contains("_sheathActionParamOne_"))
@@ -1042,7 +1041,7 @@ namespace SAI_Comment_Converter
             smartActionStrings.Add(SmartAction.SMART_ACTION_AUTO_ATTACK, "_startOrStopActionParamOne_ Attacking");
             smartActionStrings.Add(SmartAction.SMART_ACTION_COMBAT_MOVEMENT, "_enableDisableActionParamOne_ Combat Movement");
             smartActionStrings.Add(SmartAction.SMART_ACTION_SET_EVENT_PHASE, "Set Event Phase _actionParamOne_");
-            smartActionStrings.Add(SmartAction.SMART_ACTION_INC_EVENT_PHASE, "_incrementOrDecrementActionParamOne_ Phase");
+            smartActionStrings.Add(SmartAction.SMART_ACTION_INC_EVENT_PHASE, "_incrementOrDecrementPhaseActionParamsOneAndTwo_");
             smartActionStrings.Add(SmartAction.SMART_ACTION_EVADE, "Evade");
             smartActionStrings.Add(SmartAction.SMART_ACTION_FLEE_FOR_ASSIST, "Flee For Assist");
             smartActionStrings.Add(SmartAction.SMART_ACTION_CALL_GROUPEVENTHAPPENS, "Quest Credit '_questNameActionParamOne_'");

commit b44e0e6cc03a8b46cb032e1e4d38af9f326ecde0

Fix an issue caused by 6b70222b2ce099cad04fd4c2cb33ce7e5565a08f that broke SMART_ACTION_INC_EVENT_PHASE. For TC, this was fixed in https://github.com/TrinityCore/TrinityCore/commit/42a365af8be15d91dff34122090587599cb04548

SAI-Comment-Converter/Program.cs
@@ -405,7 +405,7 @@ namespace SAI_Comment_Converter
                 if (fullLine.Contains("_incrementOrDecrementPhaseActionParamsOneAndTwo_"))
                 {
                     if (smartscript.action_param1 != 0)
-                        fullLine = fullLine.Replace("_incrementOrDecrementPhaseActionParamsOneAndTwo_", "Increment Phase By " + smartscript.action_param2);
+                        fullLine = fullLine.Replace("_incrementOrDecrementPhaseActionParamsOneAndTwo_", "Increment Phase By " + smartscript.action_param1);
                     else if (smartscript.action_param2 != 0)
                         fullLine = fullLine.Replace("_incrementOrDecrementPhaseActionParamsOneAndTwo_", "Decrement Phase By " + smartscript.action_param2);
                 }